1c5b2fcfb287887c19e2ae127f8965a96cbc97eb
Matheus Alves <theusic@outlook.com>
2024-10-03 11:55:38 -0300

    Add files via upload

---
+++ biblioteca.py
@@ -0,0 +1,5 @@
+def imprime_nome(nome):
+    print(f"nome:{nome}")
+def imprimir_numeros(n):
+    for x in range(1,n+1,1):
+        print(x,end=" ")
---
+++ funções.py
@@ -0,0 +1,4 @@
+from biblioteca import imprime_nome, imprimir_numeros
+
+imprime_nome("joão")
+imprimir_numeros(10)
